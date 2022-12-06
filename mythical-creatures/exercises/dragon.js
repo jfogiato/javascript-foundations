aedb0afe32afad74c92ef5fdecdc17567d21a5ff
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-05 19:38:50 -0500

    Update dragon files with functional classes and tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,17 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.meals = 0;
+    }
+    greet() {
+        return `Hi, ${this.rider}!`;
+    }
+    eat() {
+        this.meals += 1;
+        return this.meals >= 3 ? this.hungry = false : this.hungry = true;
+    }
+}
 
+module.exports = Dragon
