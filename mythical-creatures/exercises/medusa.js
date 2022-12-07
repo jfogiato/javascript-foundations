a8164d97c78d09ec122a4de91c9bac935694b9eb
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-07 14:36:36 -0500

    Update: README file, Medusa, Person, and Statue files

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,22 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name) {
+        this.name = name;
+        this.statues = [];
+    };
+    gazeAtVictim(victim) {
+        var newStatue = new Statue(victim.name);
+        if (this.statues.length < 3) {
+            this.statues.push(newStatue);
+        } else {
+            var backToHum = new Person(this.statues[0].name, 'relieved');
+            this.statues.splice(0,1);
+            this.statues.push(newStatue);
+            return backToHum;
+        };
+    };
+};
+
+module.exports = Medusa;
