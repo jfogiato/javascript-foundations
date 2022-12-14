51e112a118033707f9c400c5bfd1127ef8486137
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-14 10:25:56 -0500

    Update: Lunchbox and Snack files

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,22 @@
 class LunchBox {
-
+    constructor(obj) {
+        this.owner = obj.owner;
+        this.material = obj.madeOf;
+        this.shape = obj.shape;
+        this.color = obj.color;
+        this.snacks = [];
+    };
+    acquireSnack(snack) {
+        this.snacks.push(snack);
+        snack.isInLunchBox = true;
+    };
+    findHealthySnacks() {
+        var healthySnacks = [];
+        for (var i = 0; i < this.snacks.length; i++) {
+            this.snacks[i].checkForHealthy() === true ? healthySnacks.push(this.snacks[i].type) : healthySnacks;
+        };
+        return healthySnacks;
+    };
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,18 @@
 class Snack {
-
+    constructor(type) {
+        this.deliciousLevel = 'extra';
+        this.type = type;
+        this.amount = 100;
+        this.isInLunchBox = false;
+        this.cuttingItClose = false;
+    };
+    getEaten() {
+        this.amount -= 10;
+        this.amount <= 20 ? this.cuttingItClose = true : this.cuttingItClose;
+    };
+    checkForHealthy() {
+        return this.type.toLowerCase().includes('fruit') ? true : false;
+    };
 }
 
 module.exports = Snack;
