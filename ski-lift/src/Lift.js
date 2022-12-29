aef69f78e108f32b8a41b26b0fb743830044ed31
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-29 14:48:20 -0500

    Upate: all ski-lift files with solutions

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,32 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+    constructor(limit) {
+        this.inService = true;
+        this.limit = limit;
+        this.skiers = [];
+        this.safetyBar = 'up';
+    }
+    admitSkier(name, ticket) {
+        if (this.skiers.length < this.limit && ticket) {
+            var skier = new Skier(name, ticket);
+            this.skiers.push(skier);
+        } else if(!ticket) {
+            return `Sorry, ${name}. You need a lift ticket!`;
+        } else {
+            return `Sorry, ${name}. Please wait for the next lift!`;
+        };
+    }
+    startLift() {
+        var dif = this.limit - this.skiers.length;
+        if (dif === 0) {
+            this.safetyBar = 'down';
+        } else if (dif === 1) {
+            return `We still need 1 more skier!`;
+        } else {
+            return `We still need ${dif} more skiers!`;
+        }
+    }
 }
 
 module.exports = Lift;
