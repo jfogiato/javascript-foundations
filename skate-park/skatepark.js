d3fbf53d1a729831924a809b355a5cb7de4f6cd2
Joe Fogiato <joe.fogiato@gmail.com>
2023-01-02 10:18:49 -0500

    Update: add additional skatepark solutions

--- skate-park/skatepark.js
+++ skate-park/skatepark.js
@@ -8,6 +8,23 @@ class Skatepark {
         this.cost = object.price || 0;
         this.occupants = [];
     }
+    admit(skater) {
+        if (this.occupants.length < 3) {
+            if (this.isPrivate) {
+                if (skater.money < this.cost) {
+                    return 'Sorry, you don\'t have enough money.';
+                }
+                skater.money -= this.cost;
+                this.occupants.push(skater);
+                return `Welcome to ${this.name}, the cost will be $${this.cost}.00.`;
+            } else {
+                this.occupants.push(skater);
+                return `Welcome to the free ${this.name} Skatepark!`;
+            }
+        } else {
+            return 'Sorry, we are at max capacity. Thank you for understanding.';
+        }
+    }    
 }
 
 module.exports = Skatepark;
